0d219052b2cc7a96c04fcbf82fac36e0842c5b71
Yash Jadhav <yash14dj@gmail.com>
2025-04-14 21:09:37 +0530

    main app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def update_label():
 update_label()
 window.mainloop()
 
-# ==============The end by github.com/kalebu ==========
+# ============================https://github.com/yashjadhav14==========================
